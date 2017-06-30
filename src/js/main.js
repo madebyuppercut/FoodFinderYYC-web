9258ffc3301e3859a358af8df62be73b8cbf88bd
Weslie Leung <you@example.com>
2017-06-30 15:00:01 -0600

    require share images in main so that webpack can pick those up

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom';
 
 import Routes from './routes';
 
+import ShareLarge from '../img/share-large.png';
+import ShareSmall from '../img/share-small.png';
+
 ReactDOM.render(
   <Routes/>,
   document.getElementById('app')
