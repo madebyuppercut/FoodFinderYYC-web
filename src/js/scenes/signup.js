a4051bbfde69034283dbbf0e691f2c2ab887009f
joshfolden <josh@bb4ck.org>
2018-03-21 18:27:02 -0600

    ...

--- src/js/scenes/signup.js
+++ src/js/scenes/signup.js
@@ -21,7 +21,7 @@ const SignUp = (props) => (
       <div className="g-r center">
         <div className="g-c-s-12-12 g-c-m-6-12 g-c-l-9-12 t-center">
           <p style={{fontSize: '1.125rem', lineHeight: 1.4}}>
-            Welcome! (test) This service will be live on July 1st. To get a reminder, please leave your email address.
+            Welcome! (test2) This service will be live on July 1st. To get a reminder, please leave your email address.
           </p>
           <SubscribeForm/>
         </div>
