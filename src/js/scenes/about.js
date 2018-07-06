e9d3ac9a8d315596a3c83fffb6db180fb1a05e5f
joshfolden <josh@bb4ck.org>
2018-07-06 14:00:31 -0600

    Update about.js

--- src/js/scenes/about.js
+++ src/js/scenes/about.js
@@ -30,6 +30,9 @@ export default class Intro extends React.Component {
             <p className="tag">
               <span className="strong">Why:</span> Because you matter, because food and health matter. Because the struggle doesn't have to be real this summer.
             </p>
+            <p className="tag">
+              <span className="strong">Donate:</span> Test...
+            </p>
           </div>
         </div>
       </div>
