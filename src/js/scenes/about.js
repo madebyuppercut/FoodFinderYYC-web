9cc4da89fb188399a42922bfb358dc3f63e26eaa
joshfolden <josh@bb4ck.org>
2018-07-06 15:56:02 -0600

    Update about.js

--- src/js/scenes/about.js
+++ src/js/scenes/about.js
@@ -31,7 +31,7 @@ export default class Intro extends React.Component {
               <span className="strong">Why:</span> Because you matter, because food and health matter. Because the struggle doesn't have to be real this summer.
             </p>
             <p className="tag">
-              <span className="strong">Donate:</span> <a href=https://www.canadahelps.org/en/dn/m/36061/donation> Donate </a>
+              <span className="strong">Donate:</span> <a href={https://www.canadahelps.org/en/dn/m/36061/donation}> Donate </a>
             </p>
           </div>
         </div>
