287fe7b883f31e28b693fe27ccae9911d3c607b0
hosseintalebi <hossein.t@getjobber.com>
2020-04-04 18:00:55 -0600

    linting

--- server.js
+++ server.js
@@ -31,5 +31,7 @@
   // static file serve
   app.use('/', express.static(__dirname + '/dist'));
   app.use('/*', express.static(__dirname + '/dist/index.html'));
-  app.listen(process.env.PORT || 3000, function() {console.log(`Server running on: http://127.0.0.1:${process.env.PORT || 3000}`)});
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on: http://127.0.0.1:${process.env.PORT || 3000}`)
+  });
 })();
